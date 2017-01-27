ec6f47a1dae04ccbd5a5cff39864326c1bec9e97
Quinntyne Brown <quinntynebrown@gmail.com>
2017-01-26 20:15:48 -0500

    boilerplate

--- QuinntyneBrownPhotography/Data/Models/Customer.cs
+++ QuinntyneBrownPhotography/Data/Models/Customer.cs
@@ -3,7 +3,10 @@ namespace QuinntyneBrownPhotography.Data.Models
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
         public bool IsDeleted { get; set; }
     }
 }
--- QuinntyneBrownPhotography/Features/Customers/CustomerApiModel.cs
+++ QuinntyneBrownPhotography/Features/Customers/CustomerApiModel.cs
@@ -3,15 +3,21 @@ using QuinntyneBrownPhotography.Data.Models;
 namespace QuinntyneBrownPhotography.Features.Customers
 {
     public class CustomerApiModel
-    {        
+    {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
 
         public static TModel FromCustomer<TModel>(Customer customer) where
             TModel : CustomerApiModel, new()
         {
             var model = new TModel();
-            model.Id = customer.Id;
+            model.Firstname = customer.Firstname;
+            model.Lastname = customer.Firstname;
+            model.EmailAddress = customer.Firstname;
+            model.PhoneNumber = customer.Firstname;
             return model;
         }
     }
